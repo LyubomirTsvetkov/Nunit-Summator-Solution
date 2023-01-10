d4e37f20d57c477ff79844ce73afeaefde38242a
LyubomirTsvetkov <105011987+LyubomirTsvetkov@users.noreply.github.com>
2023-01-10 14:55:41 +0200

    Add files via upload

---
+++ Summator/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        long result = ConsoleApp1.Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+        if (result == 1)
+        {
+            Console.WriteLine("Test pass");
+        }
+        else
+        {
+            Console.WriteLine("Test fail");
+        }
+
+        int average = ConsoleApp1.Summator.Average(new int[] { 5, 10, 15 });
+        if (average == 10)
+        {
+            Console.WriteLine("Test pass");
+        }
+        else
+        {
+            Console.WriteLine("Test fail");
+        }
+    }
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Summator
+    {
+        public static long Sum(int[] array)
+        {
+            long sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        public static int Average(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum / array.Length;
+        }
+    }
+}
